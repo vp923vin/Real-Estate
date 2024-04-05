dfdf0d4e1a699627b151d1ab84896902b7389a1c
amitcarpenter200 <amitcarpenter200@gmail.com>
2024-04-05 12:36:53 +0530

    upload path change

--- src/Routes/admin.js
+++ src/Routes/admin.js
@@ -5,7 +5,7 @@ const multer = require("multer");
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "uploads/");
+    cb(null, "public/assets/uploads/blogs/tImages");
   },
   filename: function (req, file, cb) {
     cb(null, file.originalname);
